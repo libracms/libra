9594aeb283177ac3742080c74ad000c27ec8f60b
Vitalii Nagara <duke@ejoom.com>
2013-04-22 11:56:05 +0300

    Fix Page\Mvc from v2.1.5 by removing autocomplete with RouteMatch

---
+++ autoload_classmap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Overload Zend classes to suitable for project
+ */
+return array(
+    'Zend\Navigation\Page\Mvc'                  => __DIR__ . '/src/Zend/Navigation/Page/Mvc.php',
+);
