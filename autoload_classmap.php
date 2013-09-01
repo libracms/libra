c463e75e12fb686282546a4ff3bc743179873cdb
Vitalii Nagara <duke@ejoom.com>
2013-09-01 06:53:29 +0300

    Remove overriding Page\Mvc as behaviour was fixed in ZF

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Overload Zend classes to suitable for project
  */
 return array(
-    'Zend\Navigation\Page\Mvc'                  => __DIR__ . '/src/Zend/Navigation/Page/Mvc.php',
 );
